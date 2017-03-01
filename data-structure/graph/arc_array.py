e8d7dfcd3ccc724f16309741184da5ced8a0211b
sun <huachuang20@gmail.com>
2017-03-01 16:17:14 +0800

    add arc_array

--- data-structure/graph/arc_array.py
+++ data-structure/graph/arc_array.py
@@ -1,1 +1,16 @@
 # arc Array 边集数组
+class Vertex:
+    store = []
+    def __init__(self, *value):
+        self.store = list(value)
+
+class ArcNode:
+    def __init__(self, weight,  tail, head):
+        self.weight = weight
+        self.tail = tail
+        self.head = head
+
+class Arc:
+    store = []
+    def __init__(self, *arc_nodes):
+        self.store = list(arc_nodes)
